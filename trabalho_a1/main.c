e02377db25a543f6ae0234cd42e3e1de13faabb9
AA - Queiza Sâmylle <lalaqueumy@gmail.com>
2026-06-30 21:21:19 -0300

    Update main.c

--- trabalho_a1/main.c
+++ trabalho_a1/main.c
@@ -1,23 +1,9 @@
-/*
- * main.c
- * Arquivo principal do sistema de playlist de musicas.
- * Declara o vetor de musicas e a variavel de controle,
- * e executa o loop principal do menu.
- *
- * Tema      : A1 - Playlist de Musicas
- * Disciplina: Estrutura de Dados
- * Curso     : Analise e Desenvolvimento de Sistemas
- * Instituicao: UniJorge
- * Semestre  : 3 semestre
- */
+
 
 #include <stdio.h>
 #include "playlist.h"
 
-/*
- * Declaracao do vetor de musicas e da variavel de controle.
- * Sao definidas aqui e referenciadas nos outros arquivos via extern.
- */
+
 Musica playlist[MAX_MUSICAS];
 int    quantidade = 0;
 
@@ -25,7 +11,7 @@ int main(void)
 {
     int opcao;
 
-    carregarCSV(); /* tenta carregar dados salvos anteriormente */
+    carregarCSV(); 
 
     do {
         exibirMenu();
